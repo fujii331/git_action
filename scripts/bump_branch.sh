2497746a9fae8058e48cb9a6f99e7fe509b28fb9
naoto613 <fujii140@gmail.com>
2021-01-29 10:35:41 +0900

    ts

--- scripts/bump_branch.sh
+++ scripts/bump_branch.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-git switch -c "$DEPLOY_BRANCH" "$BASE_BRANCH"
-git push origin "$DEPLOY_BRANCH" -f
-
-COMMIT_MESSAGE="stagingにデプロイ"
+BRANCH_NAME = "bump_$DEPLOY_BRANCH"
+git switch -c "$BRANCH_NAME" "$BASE_BRANCH"
+git push origin "$BRANCH_NAME"
+COMMIT_MESSAGE="patch-chore: Reflect $BASE_BRANCH branch change to $DEPLOY_BRANCH branch"
 URL="https://api.github.com/repos/$OWNER/$REPO/pulls"
 DATA="{\"title\": \"$COMMIT_MESSAGE\", \"head\": \"$BRANCH_NAME\", \"draft\": false, \"base\": \"$DEPLOY_BRANCH\"}"
 curl -d "$DATA" -X POST -s -H "Authorization: token $GITHUB_TOKEN" "$URL"
